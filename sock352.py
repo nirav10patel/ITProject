9fedef1d2c751eeb37184110a9a80677b51f1f98
Miraj Patel <mirajvpatel@gmail.com>
2019-10-12 17:41:10 -0400

    making udp socket

--- sock352.py
+++ sock352.py
@@ -7,7 +7,19 @@ import sys
 # define the UDP ports all messages are sent
 # and received from
 
+sock352PktHdrData = '!BBBBHHLLQQLL'
+udpPortTx = 0   #this is the UDPportTX we get as input from client/server to the global init() function
+udpPortRx = 0   #this is the UDPportTX we get as input from client/server to the global init() function
+udpSock    #this is the main socket we will be using with UDP
+
 def init(UDPportTx,UDPportRx):   # initialize your UDP socket here
+    #whenever you want to use globals in python, you have to declare the 'global' variables as shown below
+    global udpPortTx, udpPortRx, udpSock
+    udpPortRx = UDPportRx
+    udpPortTx = UDPportTx
+    #here we declare that we are going to be using UDP
+    udpSock = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
+    udpSock.bind('', udpPortRx)
     pass
 
 class socket:
