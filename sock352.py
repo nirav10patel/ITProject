44591a06edf25e5fc93721f82405ba9847e20bb4
Nirav Patel <nirav10patel@gmail.com>
2019-10-13 21:28:46 -0400

    real fix
    yeas

--- sock352.py
+++ sock352.py
@@ -52,7 +52,25 @@ class socket:
         return
 
     def connect(self,address):  # fill in your code here
-        global udpSock, seqNum
+        global udpSock, seqNum, header_len
+        seqNum = int(random.randint(20, 100))
+        data = self.updateStruct(header_len, seqNum, 0, 0)
+        ackServer = -1;
+        while true:
+            print("CONNECTING")
+            udpSock.sendto(data, (address[0], udpPortTx))
+            serverData = self.getData()
+            ackServer = serverData[9]
+            if ackServer == seqNum:
+                print("Got ack from server")
+                print("SUCCESSFUL CONNECT")
+                break
+            else:
+                print("The ack is: " + ackServer + " The seqNum is: " + seqNum)
+                print("FAILED CONNECT TRYING AGAIN")
+                print("ACK: " + ackServer + " SEQ: " + seqNum)
+        udpSock.connect((address[0], udpPortTx))
+        seqNum = seqNum + 1
         return
 
     def listen(self,backlog):
@@ -61,16 +79,8 @@ class socket:
     def accept(self):
         #in this method, we must use the recvfrom(), its like the linnux call
         #that is how we know that an object of class sock352 somewhere has sent something
-        global udpSock, udpPortRx, seqNum, header_len
-
-        updatedStruct = ""
-        while(true):
-            updatedStruct = self.getData()
-            if(updatedStruct[1] == 0x01):
-                seqNum = updatedStruct[8]
-                break
-
-        struct = self.updateStruct(0x04, header_len, seqNum, 0, 13)
+        global udpSock, udpPortRx, seqNum
+        while()
         (clientsocket, address) = (1,1)  # change this to your code
         return (clientsocket,address)
 
