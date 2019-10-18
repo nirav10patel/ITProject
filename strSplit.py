d5acea7adbf43dc1960a14ed5e26de86b37924d4
Miraj Patel <mirajvpatel@gmail.com>
2019-10-18 18:02:59 -0400

    deleted unnecessary files

--- strSplit.py
+++
@@ -1,9 +0,0 @@
-def main():
-    str = "catBatSatFatOr"
-    data = [str[i:i+3] for i in range(0, len(str), 3)]
-    print(len(data))
-    for i in range(0, 5):
-        print(data[i])
-
-if __name__ == "__main__":
-    main()
--- truncate.py
+++
@@ -1,6 +0,0 @@
-f = open("sendFile.txt",  "a")
-f.truncate(15)
-f.close()
-
-# f = open("sendFile.txt", "r")
-# print(f.read())
